ad02b2834588b303c777729c1b05bbb1c7d16e5c
Rahul pithadia <61902755+Rahulpith124@users.noreply.github.com>
2021-03-31 13:21:46 +0530

    Add files via upload

---
+++ Flex.js
@@ -0,0 +1,15 @@
+const panel = document.querySelectorAll('.panel');
+
+function toggleOpen() {
+    this.classList.toggle('open');
+}
+
+function toggleActivity(e) {
+    if (e.propertyName.includes('flex')) {
+        this.classList.toggle('open-active')
+    }
+
+}
+
+panel.forEach(panel => panel.addEventListener('click', toggleOpen));
+panel.forEach(panel => panel.addEventListener('transitionend', toggleActivity));
